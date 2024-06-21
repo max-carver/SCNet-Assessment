000fa433daf29bae2a120b6cece3fc3e8ba824cb
max-carver <maxevans3108@gmail.com>
2024-06-21 13:21:52 +0200

    move navlinks to separate file

---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "SA Map Stats",
+		path: "/map",
+	},
+	{
+		title: "Health Report",
+		path: "/health-report",
+	},
+];
